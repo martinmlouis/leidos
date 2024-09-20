bb1d9def0da128a993c04696f705ca81f5bc17be
Martin Lourduswamy <martin.louis@gmail.com>
2024-09-20 18:53:44 +0000

    added argocd params and script

---
+++ builds/deploy_argocd_apps.bash
@@ -0,0 +1,27 @@
+#!/usr/bin/bash
+
+set -x
+
+NAME=$1
+REPO=$2
+REVISION=$3
+NAMESPACE=$4
+DESTINATION_SERVER=$5
+LABEL=$6
+ARGOCD_SERVER=$6
+
+argocd app create ${NAME} --repo ${REPO} \
+        --insecure \
+        --helm-chart ${NAME} \
+        --revision ${REVISION}  \
+        --dest-namespace ${NAMESPACE}\
+        --dest-server ${DESTINATION_SERVER} \
+        --sync-policy automatic \
+        --name ${NAME} \
+        --label ${LABEL} \
+        --self-heal \
+        --sync-option Prune=true \
+        --sync-retry-limit 10 \
+        --validate \
+        --server ${ARGOCD_SERVER}
+
