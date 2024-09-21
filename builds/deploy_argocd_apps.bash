f6facda0c58d6ed8a5e5e2352dc786e6157a28db
Martin Lourduswamy <martin.louis@gmail.com>
2024-09-21 21:16:24 +0000

    test

--- builds/deploy_argocd_apps.bash
+++ builds/deploy_argocd_apps.bash
@@ -6,7 +6,7 @@ catch_delete_deployment() {
   #argocd app delete ${NAME} --wait --yes --propagation-policy foreground --cascade 
   argocd login ${ARGOCD_SERVER} --name "admin" --password "FTzSauYgabur1-S7" --grpc-web --insecure --username admin
   sleep 7
-  argocd app delete ${NAME} \
+  argocd app delete argo-cd/${NAME} \
 	--app-namespace ${NAMESPACE} \
 	--yes --propagation-policy foreground --cascade \
         --insecure \
