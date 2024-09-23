dcb06e5336effc2ccef3beecd0680505306f43b5
Martin Lourduswamy <martin.louis@gmail.com>
2024-09-23 19:54:53 +0000

    test

--- builds/deploy_argocd_apps_git.bash
+++ builds/deploy_argocd_apps_git.bash
@@ -12,27 +12,27 @@ declare LABEL=$6
 declare ARGOCD_SERVER=$7
 declare VALUES_FILE=$8
 
-/usr/local/bin/argocd login ${ARGOCD_SERVER} --name "admin" --password "FTzSauYgabur1-S7" --grpc-web --insecure --username admin
+/usr/local/bin/argocd login "${ARGOCD_SERVER}" --name "admin" --password "FTzSauYgabur1-S7" --grpc-web --insecure --username admin
 
 /bin/sleep 7
 
-/usr/local/bin/argocd app set ${NAME} --values ${PATH}/${VALUES_FILE}
+/usr/local/bin/argocd app set "${NAME}" --values "${PATH}/${VALUES_FILE}"
 
-/usr/local/bin/argocd app create ${NAME} --repo ${REPO} \
+/usr/local/bin/argocd app create "${NAME}" --repo "${REPO}" \
         --insecure \
-        --path ${PATH} \
-        --dest-namespace ${NAMESPACE}\
-        --dest-server ${DESTINATION_SERVER} \
+        --path "${PATH}" \
+        --dest-namespace "${NAMESPACE}"\
+        --dest-server "${DESTINATION_SERVER}" \
         --sync-policy automatic \
         --self-heal \
-        --sync-option Prune=true \
-        --sync-option CreateNamespace=true \
+        --sync-option "Prune=true" \
+        --sync-option "CreateNamespace=true" \
         --sync-retry-limit 10 \
-        --name ${NAME} \
-        --label ${LABEL} \
+        --name "${NAME}" \
+        --label "${LABEL}" \
         --validate \
 	--upsert \
-        --server ${ARGOCD_SERVER}
+        --server "${ARGOCD_SERVER}"
 
 exit 0
 
