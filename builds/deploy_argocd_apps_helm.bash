8cc9843b2b2e6cfa57b232320ab62533052d5270
Martin Lourduswamy <martin.louis@gmail.com>
2024-09-25 12:24:59 +0000

    test

--- builds/deploy_argocd_apps_helm.bash
+++ builds/deploy_argocd_apps_helm.bash
@@ -35,10 +35,11 @@ argocd login ${ARGOCD_SERVER} --name "admin" --password "UEdnzGuEOq-3nnqn" --grp
 
 sleep 7
 
-#if [ "$(argocd app list |grep "${NAME}"|wc -l)" -gt  "0" ]; then
-#  argocd app patch ${NAME} --patch "{'spec': { 'source': { 'targetRevision': '${REVISION}' } }}" --type merge
-#  sleep 300
-#fi
+if [ "$(argocd app list |grep "${NAME}"|wc -l)" -gt  "0" ]; then
+  #argocd app patch ${NAME} --patch "{'spec': { 'source': { 'targetRevision': '${REVISION}' } }}" --type merge
+  argocd app delete ${NAME}
+  sleep 300
+fi
 
 argocd app create ${NAME} --repo ${REPO} \
 	--values "${ENVIRONMENT}-values.yaml" \
