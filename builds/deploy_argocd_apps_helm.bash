0400908bd0031357f2d7073bfe95266ea70bd98c
Martin Lourduswamy <martin.louis@gmail.com>
2024-10-01 23:24:22 +0000

    test

--- builds/deploy_argocd_apps_helm.bash
+++ builds/deploy_argocd_apps_helm.bash
@@ -31,7 +31,7 @@ elif [ $(echo "${ARGOCD_SERVER}"|grep prod|wc -l) > 0 ]; then
   ENVIRONMENT="prod"
 fi
 
-argocd login ${ARGOCD_SERVER} --name "admin" --password "e85XkdwaTkPNZUln" --grpc-web --insecure --username admin
+argocd login ${ARGOCD_SERVER} --name "admin" --password "bx8BbAA891qxoQ6O" --grpc-web --insecure --username admin
 
 sleep 7
 
